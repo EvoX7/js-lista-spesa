72614ad20698f6664e0a4d457d30436568a5fe9a
EvoX7 <creativesoultouch@gmail.com>
2022-06-21 16:11:16 +0200

    My Shopping List

---
+++ js/main.js
@@ -0,0 +1,30 @@
+// My ShoppingList
+const shoppingList = [
+    "Pane",
+    "Uova",
+    "Maionese",
+    "Cereali",
+    "Verdure",
+    "Coca cola",
+    "Pasta",
+    "Pollo",
+    "Fagiolini",
+    "Insalata",
+    "Banane"
+  ];
+  console.log(shoppingList);
+  
+  // Defining output
+  const myCart = document.getElementById("shopping-list");
+  
+  // While Loop
+  let i = 0;
+  
+  while (i < shoppingList.length) {
+    console.log(shoppingList[i]);
+    const newItem = document.createElement("li");
+    newItem.innerHTML = shoppingList[i];
+    myCart.append(newItem);
+    i++;
+  }
+  
